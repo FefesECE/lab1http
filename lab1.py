e130723de376ac329f4cb04aa322d2bd7c1d39b9
Δημητρης Φεφες <161204187+FefesECE@users.noreply.github.com>
2024-02-26 21:33:05 +0200

    Add files via upload
    Ολοκληρωση 1ης εργαστηριακης ασκησης

---
+++ lab1.py
@@ -0,0 +1,30 @@
+import requests
+while 1:
+ url = input('Παρακαλω δωστε ενα url:')
+ if url == '': break
+ if not url.startswith('http'):
+    url = 'http://'+ url
+ try:
+    with requests.get(url) as response:
+        print("\nRESPONSE STATUS: ", response.status_code)
+        print("RESPONSE HEADER")
+        for key, value in response.headers.items():
+            print("{:30s} {}".format(key, value))
+        
+        server = response.headers.get('Server')
+        
+        if server:
+           print(f'Ο server που χρησιμοποιειται ειναι {server}')
+        else:
+           print('Δεν εντοπιζεται ο server :(')
+        
+        cookies = response.headers.get('set-cookie')
+        if cookies:
+            print('Παρακατω φαινονται τα cookies: \n',cookies)
+        else:
+           print('Δεν υπαρχουν cookies :(')
+        
+
+
+ except:
+    print('Λαθος,ισως δεν ειναι σωστο το link:', url)
